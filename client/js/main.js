de2e2399ecbde5907ae2a66b2933e42514c5255c
Brandon Brown <brandon.brown@ymail.com>
2022-01-18 04:26:14 -0800

    Basic skeleton app tracks movement and sends values to Wekinator

---
+++ client/js/main.js
@@ -0,0 +1,171 @@
+import "regenerator-runtime/runtime"; // So that Parcel will support Promise and async/await
+import * as tf from "@tensorflow/tfjs-core";
+import "@tensorflow/tfjs-backend-webgl";
+import * as poseDetection from "@tensorflow-models/pose-detection";
+
+//from https://openprocessing.org/sketch/1236523/pc/afKoTxb1
+//info about skeleton https://discuss.tensorflow.org/t/drawing-skeleton-with-movenet-on-p5js-with-movenet-tensorflow-js-poses-are-detected-and-working/3102/5
+window.p5js = window.p5;
+delete window.p5;
+
+const socket = io("http://localhost:3000");
+
+const sketch = new p5js((p5) => {
+  const ScoreThreshold = 0.4;
+
+  let detector;
+  let poses;
+  let video;
+  let screenAspect;
+  let videoAspect;
+
+  p5.setup = async function setup() {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    screenAspect = p5.windowWidth / p5.windowHeight;
+
+    const detectorConfig = {
+      modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+    };
+
+    detector = await poseDetection.createDetector(
+      poseDetection.SupportedModels.MoveNet,
+      detectorConfig
+    );
+    console.log("Pose Detector Initialized");
+
+    video = p5.createCapture(p5.VIDEO, getPosesRecursively);
+    videoAspect = video.width / video.height;
+    // video.hide();
+    console.log("setup complete");
+  };
+
+  async function getPosesRecursively() {
+    poses = await detector.estimatePoses(video.elt);
+    setTimeout(getPosesRecursively, 100);
+    console.log("poses:", poses);
+    socket.emit("poses", poses);
+  }
+
+  // // A list of pairs of either keypoint indices or sub lists of keypoint indicies
+  // // Each pair defines an edge in the skeleton "graph"
+  // // When a pair contains a sublist, that is meant to represent the average of two keypoints
+  // const skeleton = [
+  //   [0, 1],
+  //   [0, 2],
+  //   [1, 3],
+  //   [2, 4],
+  //   [0, [6, 5]],
+  //   [6, 5],
+  //   [6, 12],
+  //   [5, 11],
+  //   [5, 7],
+  //   [6, 8],
+  //   [7, 9],
+  //   [8, 10],
+  //   [
+  //     [5, 6],
+  //     [11, 12],
+  //   ],
+  //   [[11, 12], 11],
+  //   [[11, 12], 12],
+  //   [11, 13],
+  //   [12, 14],
+  //   [13, 15],
+  //   [14, 16],
+  // ];
+
+  // function getKeypointForEdgeVertex(keypoints, vertex) {
+  //   if (typeof vertex === "number") {
+  //     const { x, y, score } = keypoints[vertex];
+  //     if (score > ScoreThreshold) {
+  //       return { x, y };
+  //     }
+  //   } else if (vertex instanceof Array) {
+  //     const points = vertex.map((v) => keypoints[v]);
+  //     if (points.every((kp) => kp.score > ScoreThreshold)) {
+  //       const { x, y } =
+  //         // Average the points
+  //         points.reduce(
+  //           (acc, v) => ({
+  //             x: (acc.x * acc.w + v.x) / (acc.w + 1),
+  //             y: (acc.y * acc.w + v.y) / (acc.w + 1),
+  //             w: acc.w + 1,
+  //           }),
+  //           { x: 0, y: 0, w: 0 }
+  //         );
+  //       return { x, y };
+  //     }
+  //   }
+  // }
+
+  // p5.draw = function draw() {
+  //   p5.background(0, 0, 0, 10);
+  //   if (video) {
+  //     let vw, vh;
+  //     // This isn't valid during setup() for some reason
+  //     videoAspect = video.width / video.height;
+  //     if (screenAspect >= videoAspect) {
+  //       // The screen is wider than the video
+  //       vh = p5.height;
+  //       vw = p5.height * videoAspect;
+  //     } else {
+  //       // The video is wider than the screen
+  //       vw = p5.width;
+  //       vh = p5.width / videoAspect;
+  //     }
+  //     p5.push();
+  //     // Mirror the video
+  //     p5.scale(-1, 1);
+  //     p5.translate(-vw, 0);
+  //     p5.image(video, 0, 0, vw, vh);
+  //     p5.pop();
+  //     // One way to adjust the skeleton to match the video size would be to use
+  //     // scale():
+  //     //     scale(vw / video.width, vh / video.height);
+  //     // However this scales stroke an text size as well. And if we wanted to flip
+  //     // the skeleton horinontally (using a negative scaling factor in the x axis),
+  //     // this would also flip the text.
+  //     //
+  //     // To only adjust position we can use the map() function like so:
+  //     //     map(x, 0, video.width, vw, 0)
+  //     // Note that the ordering of the input range and output range is flipped in
+  //     // order to mirror the skeleton.
+
+  //     // helper functions
+  //     const mapX = (x) => p5.map(x, 0, video.width, vw, 0);
+  //     const mapY = (y) => p5.map(y, 0, video.height, 0, vh);
+
+  //     if (poses && poses.length > 0) {
+  //       //console.log(poses[0].keypoints.length)
+  //       //console.log(poses[0].keypoints[0].x);
+  //       p5.stroke(0, 255, 255);
+  //       p5.strokeWeight(2);
+  //       for (let edge of skeleton) {
+  //         let start = getKeypointForEdgeVertex(poses[0].keypoints, edge[0]);
+  //         let end = getKeypointForEdgeVertex(poses[0].keypoints, edge[1]);
+
+  //         if (start && end) {
+  //           p5.line(mapX(start.x), mapY(start.y), mapX(end.x), mapY(end.y));
+  //         }
+  //       }
+
+  //       for (let i = 0; i < poses[0].keypoints.length; i++) {
+  //         const { x, y, score } = poses[0].keypoints[i];
+  //         // console.log(kp);
+  //         if (score > ScoreThreshold) {
+  //           p5.fill(255, 0, 255);
+  //           p5.stroke(0, 255, 255);
+  //           p5.strokeWeight(2);
+  //           p5.circle(mapX(x), mapY(y), 15);
+
+  //           p5.push();
+  //           // fill('red');
+  //           // noStroke();
+  //           // text(`${i}`, mapX(x), mapY(y));
+  //           p5.pop();
+  //         }
+  //       }
+  //     }
+  //   }
+  // };
+}, document.getElementById("p5-sketch"));
---
+++ server/server.js
@@ -0,0 +1,143 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const httpServer = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:1234",
+  },
+});
+const osc = require("osc-min");
+const dgram = require("dgram");
+const udp = dgram.createSocket("udp4");
+
+io.on("connection", (socket) => {
+  socket.on("poses", (poses) => {
+    if (!poses) return;
+    sendPoseToWekinator(poses[0]);
+  });
+});
+
+// Examples of send OSC to Wekinator
+// https://github.com/parcodiyellowstone/posenet-authentication/blob/master/train/app.js
+// https://github.com/noisyneuron/wekOsc/blob/master/server.js
+const sendPoseToWekinator = function (rawPoseData) {
+  if (!rawPoseData) return;
+
+  const flattenedPoseObject = {};
+  rawPoseData.keypoints.forEach((k) => {
+    flattenedPoseObject[k.name] = {};
+    flattenedPoseObject[k.name]["y"] = k.y;
+    flattenedPoseObject[k.name]["x"] = k.x;
+  });
+
+  const args = [
+    { type: "float", value: flattenedPoseObject.nose.y },
+    { type: "float", value: flattenedPoseObject.nose.x },
+    { type: "float", value: flattenedPoseObject.left_eye.y },
+    { type: "float", value: flattenedPoseObject.left_eye.x },
+    { type: "float", value: flattenedPoseObject.right_eye.y },
+    { type: "float", value: flattenedPoseObject.right_eye.x },
+    { type: "float", value: flattenedPoseObject.left_ear.y },
+    { type: "float", value: flattenedPoseObject.left_ear.x },
+    { type: "float", value: flattenedPoseObject.right_ear.y },
+    { type: "float", value: flattenedPoseObject.right_ear.x },
+    { type: "float", value: flattenedPoseObject.left_shoulder.y },
+    { type: "float", value: flattenedPoseObject.left_shoulder.x },
+    { type: "float", value: flattenedPoseObject.right_shoulder.y },
+    { type: "float", value: flattenedPoseObject.right_shoulder.x },
+    { type: "float", value: flattenedPoseObject.left_elbow.y },
+    { type: "float", value: flattenedPoseObject.left_elbow.x },
+    { type: "float", value: flattenedPoseObject.right_elbow.y },
+    { type: "float", value: flattenedPoseObject.right_elbow.x },
+    { type: "float", value: flattenedPoseObject.left_wrist.y },
+    { type: "float", value: flattenedPoseObject.left_wrist.x },
+    { type: "float", value: flattenedPoseObject.right_wrist.y },
+    { type: "float", value: flattenedPoseObject.right_wrist.x },
+    { type: "float", value: flattenedPoseObject.left_hip.y },
+    { type: "float", value: flattenedPoseObject.left_hip.x },
+    { type: "float", value: flattenedPoseObject.right_hip.y },
+    { type: "float", value: flattenedPoseObject.right_hip.x },
+    { type: "float", value: flattenedPoseObject.left_knee.y },
+    { type: "float", value: flattenedPoseObject.left_knee.x },
+    { type: "float", value: flattenedPoseObject.right_knee.y },
+    { type: "float", value: flattenedPoseObject.right_knee.x },
+    { type: "float", value: flattenedPoseObject.left_ankle.y },
+    { type: "float", value: flattenedPoseObject.left_ankle.x },
+    { type: "float", value: flattenedPoseObject.right_ankle.y },
+    { type: "float", value: flattenedPoseObject.right_ankle.x },
+  ];
+  console.log("right wrist X = ", rawPoseData.keypoints[10].x);
+  console.log("nose wrist Y = ", rawPoseData.keypoints[10].y);
+  console.log("");
+
+  // Takes a JavaScript representation of an OSC packet and returns a Node.js buffer
+  // https://www.npmjs.com/package/osc-min
+
+  // By default, Wekinator listens for its input messages on port 6448.
+  // The default input message is /wek/inputs and each input must be sent as
+  // a float within this message.
+  // http://www.wekinator.org/detailed-instructions/
+  const buffer = osc.toBuffer({
+    address: "/wek/inputs",
+    args: args,
+  });
+
+  // Broadcasts a datagram on the socket. For connectionless sockets, the
+  // destination port and address must be specified. Connected sockets, on
+  // the other hand, will use their associated remote endpoint, so the port
+  // and address arguments must not be set.
+  //  The msg argument contains the message to be sent. Depending on its type,
+  // different behavior can apply. If msg is a Buffer, any TypedArray or a
+  // DataView, the offset and length specify the offset within the Buffer
+  // where the message begins and the number of bytes in the message,
+  // respectively.
+  // https://nodejs.org/api/dgram.html#socketsendmsg-offset-length-port-address-callback
+  return udp.send(buffer, 0, buffer.length, 6448, "localhost");
+};
+
+httpServer.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const remoteIp = "127.0.0.1";
+// const remotePort = 3333;
+
+/*
+ using ip 192.168.1.150 .. change this for your setup
+  in this file as well as index.html
+  if you're using your browser locally this can be set to localhost
+ */
+
+// httpServer.listen(3000);
+
+// sendHeartbeat = function (x, y) {
+//   const buf = osc.toBuffer({
+//     address: "/wek/inputs",
+//     args: [
+//       { type: "float", value: x },
+//       { type: "float", value: y },
+//       { type: "float", value: Math.random() },
+//     ],
+//   });
+
+//   return udp.send(buf, 0, buf.length, remotePort, "192.168.1.150");
+// };
+
+// io.on("connection", function (socket) {
+//   socket.emit("news", { hello: "world" });
+//   socket.on("mouseMoveEvent", function (data) {
+//     console.log(data);
+//     sendHeartbeat(data.x, data.y);
+//   });
+// });
+
+// app.use(express.static(path.join(__dirname, "../client/dist/index.html")));
+
+// app.get("/", function (req, res) {
+//   res.sendfile(path.join(__dirname, "../client/dist/index.html"));
+// });
